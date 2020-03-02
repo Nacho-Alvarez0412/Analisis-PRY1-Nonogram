12d81a7aaca292343ed4b0d05cc7aef05f0e7445
Ignacio Alvarez <52050956+Nacho-Alvarez0412@users.noreply.github.com>
2020-03-02 16:41:15 -0600

    Basic UI created

---
+++ Nonogram/Assets/Scripts/InGameScripts.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.SceneManagement;
+
+public class InGameScripts : MonoBehaviour
+{
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Nonogram/Assets/Scripts/MainMenuScripts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScripts : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void exit()
+    {
+        Debug.Log("Quitted Program!");
+        Application.Quit();
+    }
+    
+    public void loadNonogram()
+    {
+        Debug.Log("Loading Nonogram!!!");
+    }
+}
