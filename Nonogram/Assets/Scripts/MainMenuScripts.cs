34ae9e48d1096e483309e52967c928487e86c8ae
SebasG <gamboasebas@gmail.com>
2020-03-02 18:12:56 -0600

    Escoger Archivo para cargar nonoogram

--- Nonogram/Assets/Scripts/MainMenuScripts.cs
+++ Nonogram/Assets/Scripts/MainMenuScripts.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -19,5 +21,28 @@ public class MainMenuScripts : MonoBehaviour
     public void loadNonogram()
     {
         Debug.Log("Loading Nonogram!!!");
+        string path = EditorUtility.OpenFilePanel("Choose txt file", "","txt");
+        readTextFile(path);
     }
+    
+    public void readTextFile(string file_path)
+    {
+        StreamReader inp_stm = new StreamReader(file_path);
+        bool first = true;
+        int[][] matrix;
+
+        while(!inp_stm.EndOfStream)
+        {
+            string inp_ln = inp_stm.ReadLine( );
+
+            if (first)
+            {
+                //matrix = new int[][];
+            }
+            Debug.Log(inp_ln);
+        }
+
+        inp_stm.Close( );  
+    }
+
 }
